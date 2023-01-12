bf38f3faafc89706d23458e75efb4baf73b4d7cb
Alexey <shiry-1980@mail.ru>
2023-01-12 13:11:02 +0300

    t1.9

---
+++ dz9/task1.9/Program.cs
@@ -0,0 +1,24 @@
+﻿void SumNumber(int n, int m)
+{
+    Console.Write($"Сумму натуральных чисел от {n} до {m} равна = {Sum(n - 1, m)}");
+}
+
+int Sum(int n, int m)
+{
+    int res = n;
+    if (n == m)
+        return 0;
+    else
+    {
+        n++;
+        res = n + Sum(n, m);
+        return res;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+SumNumber(n, m);
